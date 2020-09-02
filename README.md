b5f8cd2c8355985fd1bf8ed0de7e7197df677a20
winnies26 <winnie.sun.26@gmail.com>
2020-09-02 11:50:15 -0700

    Update file name and remove comments

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# School-logIn-Script
+# DUO Password Generator
+
+This Password Generator auto-copy a new DUO Password for you to authenticate DUO Two-Factor Authentication on your PC. You can paste in the generated password into the prompt without the need to approving the request on your phone. 
+
+## Usage
+
+```bash
+python generator.py
+```
---
+++ generator.py
@@ -0,0 +1,14 @@
+import pyperclip
+new_s=""
+with open("/Users/winnie26/Documents/SMS_script/password.txt") as f:
+    lines = f.read().splitlines()
+    if not lines:
+        print("**TEXT YOURSELF NEW PASSWORD!**") 
+        exit()
+    to_list = lines[0].split()
+    res = to_list.pop(0)
+    pyperclip.copy(res)
+    new_s = " ".join(to_list)
+with open("/Users/winnie26/Documents/SMS_script/sms.txt",'w') as writer:
+    writer.write(new_s)
+exit()
